72b8aa4716dc7da9aefd05b65bd9a4119ecc108e
CORONA <ValenGamer96@gmail.com>
2025-05-31 05:59:52 +0000

    error fixed

--- src/components/resource/ResourceFilesTabContent.tsx
+++ src/components/resource/ResourceFilesTabContent.tsx
@@ -14,6 +14,7 @@ import { format } from 'date-fns';
 interface ResourceFilesTabContentProps {
   files: ResourceFile[];
   allChangelogEntries?: ChangelogEntry[];
+  resourceSlug: string; // Add resourceSlug here
 }
 
 const CLEAR_FILTER_VALUE = "_ANY_"; 
